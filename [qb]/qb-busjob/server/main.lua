2226b80b4b55e6f7b2dd1e2ace8d45e168781d9b
NLRPdev <110008183+NLRPdev@users.noreply.github.com>
2022-07-25 23:11:39 -0700

    econ
    updated math function to remove the stupid way it was doing it

--- [qb]/qb-busjob/server/main.lua
+++ [qb]/qb-busjob/server/main.lua
@@ -18,7 +18,7 @@ RegisterNetEvent('qb-busjob:server:NpcPay', function(Payment)
         if NearBus(src) then
             local randomAmount = math.random(1, 5)
             local r1, r2 = math.random(1, 5), math.random(1, 5)
-            if randomAmount == r1 or randomAmount == r2 then Payment = Payment + math.random(20, 35) end
+            if randomAmount == r1 or randomAmount == r2 then Payment = math.random(20, 35) end
             Player.Functions.AddMoney('cash', Payment)
         else
             DropPlayer(src, 'Attempting To Exploit')
