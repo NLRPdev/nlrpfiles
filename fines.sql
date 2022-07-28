08503a43605da94c5081af8e2a052f1397e3a866
NLRPdev <110008183+NLRPdev@users.noreply.github.com>
2022-07-28 12:44:13 -0700

    fines

---
+++ fines.sql
@@ -0,0 +1,13 @@
+INSERT INTO `mdt_fines` (`id`, `code`, `name`, `amount`, `createdAt`) VALUES
+(NULL, '1-10', 'Speed limit', 5000, NULL),
+(NULL, '1-11', 'Reckless driving', 2500, NULL),
+(NULL, '1-12', 'Seatbelt', 3000, NULL),
+(NULL, '1-13', 'Violence', 2000, NULL),
+(NULL, '1-14', 'Weapons in evidence', 3000, NULL),
+(NULL, '1-15', 'Running on stop sign', 10000, NULL),
+(NULL, '1-16', 'Ignoring a yield sign', 8000, NULL),
+(NULL, '1-17', 'Speeding 1-10mph over limit', 2000, NULL),
+(NULL, '1-18', 'Speeding 11-20mph over limit', 3000, NULL),
+(NULL, '1-19', 'Speeding 21-30mph over limit', 4000, NULL),
+(NULL, '1-20', 'Speeding 31-40mph over limit', 5000, NULL),
+(NULL, '1-21', 'Speeding more than 40mph over limit', 8000, NULL);
